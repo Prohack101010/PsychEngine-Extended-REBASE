3c6089fbbf8cfa3a354854c081a0264235bfad98
Prohack101010 <miracberaber@gmail.com>
2024-07-20 22:50:55 +0300

    Public Var

--- source/MusicBeatSubstate.hx
+++ source/MusicBeatSubstate.hx
@@ -66,8 +66,7 @@ class MusicBeatSubstate extends FlxSubState
 	
 	override function destroy() {
 		#if android
-		var OpenedCopyState = CopyStage.CopyStateOpened;
-		if (!OpenedCopyState) {
+		if (!CopyStateOpened) {
 		controls.removeFlxInput(trackedinputsUI);
 		controls.removeFlxInput(trackedinputsNOTES);
 		}
