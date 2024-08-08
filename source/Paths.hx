632b7647e151d0f25db9385d6c558ea88596f89c
Prohack101010 <miracberaber@gmail.com>
2024-08-08 16:34:29 +0300

    Add files via upload

--- source/Paths.hx
+++ source/Paths.hx
@@ -228,8 +228,9 @@ class Paths
 	    var diff = CoolUtil.getDifficultyFilePath(curDifficulty);
 	    var songKey:String = '${formatToSongPath(song)}/Voices';
 	    
-	    if (FileSystem.exists('${formatToSongPath(song)}/Voices' + '${CoolUtil.getDifficultyFilePath(curDifficulty)}')
-		    songKey = '${formatToSongPath(song)}/Voices' + '${CoolUtil.getDifficultyFilePath(curDifficulty)}';
+	    if (FileSystem.exists('${formatToSongPath(song)}/Voices-$diff') {
+		    songKey = '${formatToSongPath(song)}/Voices-$diff';
+		}
 		    
 		var voices = returnSound('songs', songKey);
 		return voices;
@@ -240,8 +241,9 @@ class Paths
 	    var diff = CoolUtil.getDifficultyFilePath(curDifficulty);
 	    var songKey:String = '${formatToSongPath(song)}/Inst';
 	    
-	    if (FileSystem.exists('${formatToSongPath(song)}/Inst' + '${CoolUtil.getDifficultyFilePath(curDifficulty)}')
-		    songKey = '${formatToSongPath(song)}/Inst' + '${CoolUtil.getDifficultyFilePath(curDifficulty)}';
+	    if (FileSystem.exists('${formatToSongPath(song)}/Inst-$diff') {
+		    songKey = '${formatToSongPath(song)}/Inst-$diff';
+		}
 		    
 		var inst = returnSound('songs', songKey);
 		return inst;
