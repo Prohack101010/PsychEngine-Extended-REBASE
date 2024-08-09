22a627b89f0ac45a104bdef95a4f14b57460d5ec
Prohack101010 <miracberaber@gmail.com>
2024-08-09 06:00:56 +0300

    Add files via upload

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -3961,7 +3961,6 @@ class ModchartAnimateSprite extends FlxAnimate
 	public function new(?x:Float = 0, ?y:Float = 0, ?path:String, ?settings:FlxAnimate.Settings)
 	{
 		super(x, y, path, settings);
-		antialiasing = ClientPrefs.data.antialiasing;
 	}
 
 	public function playAnim(name:String, forced:Bool = false, ?reverse:Bool = false, ?startFrame:Int = 0)
