b7b7543cedc00bcc63f5f1df8becd383806a67cb
Prohack101010 <miracberaber@gmail.com>
2024-08-12 23:58:03 +0300

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -54,7 +54,7 @@ import flixel.effects.particles.FlxEmitter;
 import flixel.effects.particles.FlxParticle;
 import flixel.util.FlxSave;
 import flixel.animation.FlxAnimationController;
-import animateatlasold.AtlasFrameMaker;
+import animateatlas.AtlasFrameMaker;
 import Achievements;
 import StageData;
 import FunkinLua;
