6e962e4a00e69afc9b0db1de3ab47ba86ce88f0c
Prohack101010 <miracberaber@gmail.com>
2024-08-27 16:46:37 +0300

    idk

--- source/extras/states/FreeplayStateNOVA.hx
+++ source/extras/states/FreeplayStateNOVA.hx
@@ -599,7 +599,12 @@ class FreeplayStateNOVA extends MusicBeatState
 
 				if (PlayState.SONG.needsVoices)
 				{
-					vocals = new FlxSound().loadEmbedded(Paths.voices(PlayState.SONG.song));
+				    vocals = new FlxSound();
+        		    try
+        		    {
+        			    vocals = new FlxSound().loadEmbedded(Paths.voices(PlayState.SONG.song));
+        			}
+        			catch(e:Dynamic) {}
 					FlxG.sound.list.add(vocals);
 					vocals.persist = vocals.looped = true;
 					vocals.volume = 0.8;
