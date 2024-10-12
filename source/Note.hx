5a242d2e848eb9382c7ef460c2ea0eaded255265
Prohack101010 <miracberaber@gmail.com>
2024-10-12 09:01:42 +0300

    more fix

--- source/Note.hx
+++ source/Note.hx
@@ -259,11 +259,11 @@ class Note extends FlxSprite
 		if(texture.length < 1) {
 			skin = PlayState.SONG.arrowSkin;
 			if(skin == null || skin.length < 1) {
-			    if (ClientPrefs.ChangeSkin){
-				    skin = 'NoteSkin/' + ClientPrefs.NoteSkinName;
+			    if (ClientPrefs.NoteSkin != 'original'){
+				skin = 'NoteSkin/' + ClientPrefs.NoteSkin;
 				}
 				else{
-				    skin = 'NOTE_assets';
+				skin = 'NOTE_assets';
 				}
 			}
 		}
@@ -331,7 +331,7 @@ class Note extends FlxSprite
 
 		if (isSustainNote)
 		{
-			//animation.addByPrefix('purpleholdend', 'pruple end hold'); // ?????
+			animation.addByPrefix('purpleholdend', 'pruple end hold'); // ?????
 			animation.addByPrefix(colArray[noteData] + 'holdend', colArray[noteData] + ' hold end');
 			animation.addByPrefix(colArray[noteData] + 'hold', colArray[noteData] + ' hold piece');
 		}
